f2e63aeb44e7e86bd0d9cfcb12363b6f0ff153b0
aditya kumar <a6301216@gmail.com>
2026-06-27 21:44:52 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (87.26%) - LeetSync

---
+++ 1217-relative-sort-array/relative-sort-array.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        vector<int> freq(1001, 0);
+
+        // Count frequency of elements in arr1
+        for (int num : arr1)
+            freq[num]++;
+
+        vector<int> ans;
+
+        // Place elements according to arr2
+        for (int num : arr2) {
+            while (freq[num] > 0) {
+                ans.push_back(num);
+                freq[num]--;
+            }
+        }
+
+        // Place remaining elements in ascending order
+        for (int i = 0; i <= 1000; i++) {
+            while (freq[i] > 0) {
+                ans.push_back(i);
+                freq[i]--;
+            }
+        }
+
+        return ans;
+    }
+};
